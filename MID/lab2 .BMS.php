1586b7b9cc1e1f59f4082d222cb309a329a0d70b
Saafayat Hasan <hedoyhasan111@gmail.com>
2025-07-26 10:52:04 +0600

    add new file

---
+++ MID/lab2 .BMS.php
@@ -0,0 +1,65 @@
+<!DOCTYPE>
+<html>
+  <center>
+    <title>Bank Management System</title>
+    </center>
+    
+    <body>
+        <form>
+            <table>
+                <tr>
+                    <td><label>First Name:</label></td>
+                    <td><input type = "text" name = "fname"></td>
+                </tr>
+                <tr>
+                    <td><label>Last Name:</label></td>
+                    <td><input type = "text" name = "lname"></td>
+                </tr>
+                <tr>
+                    <td><label>Password:</label></td>
+                    <td><input type = "password" name = "pass"></td>
+                </tr>
+                <tr>
+                    <td><label>Date of Birth:</label></td>
+                    <td><input type = "date" name = "dob"></td>
+                </tr>
+                <tr>
+                    <td><label>E-mail:</label></td>
+                    <td><input type = "text" name = "email"></td>
+                </tr>
+                <tr>
+                    <td><label>Account type:</label></td>
+                  
+                    <td><select name="account">
+                      <option>Savings</option>
+                      <option>Current</option>
+                      <option>Fixed Deposit</option> <td>
+                 </tr>
+                <tr>
+
+
+                 </tr>
+                <tr>
+                    <td><label>Mobil Number:</label></td>
+                    <td><input type = "mobil" name = "dob"></td>
+                </tr>
+                <tr>
+
+                    <td><label>Gender:</label></td>
+                    <td><input type = "radio" id = "male" name = "gender" value = "Male">
+                        <label for="male">Male</label>
+                        <input type = "radio" id = "female" name = "gender">
+                        <label for="female">Female</label></td>
+                </tr>
+                <tr>
+                    <td><label>National ID:</label></td>
+                    <td><input type = "NID" name = "dob"></td>
+                </tr>
+                <tr>
+
+            </table>
+ 
+        </form>
+    </body>
+</html>
+ 
